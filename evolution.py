6cdde628ca6039e001e70a65bcc961f0baeb28fb
Eduardo Di Santi <eduardodisanti@yahoo.co.uk>
2022-08-31 08:18:35 +0200

    ADD -- EVOLUTIONARY STRATEGY IMPLEMENTATION

--- evolution.py
+++ evolution.py
@@ -3,9 +3,12 @@ import numpy as np
 
 class Evolution():
     
-    def __init__(self, num_parents=10, mutation_prob=0.05):
+    def __init__(self, initial_params=2048, num_parents=10, mutation_prob=0.05, learning_rate=0.01,sigma=1):
         self.parents = num_parents
         self.mutation_prob = mutation_prob
+        self.learning_rate = learning_rate
+        self.sigma = sigma
+        self.initial_params = initial_params
         
     
     def fitness(self, rewards):
@@ -15,5 +18,23 @@ class Evolution():
         return fitness
     
     def evolve(self, population, rewards):
+        best_genes = np.argsort(rewards)[-self.parents:].flatten()
+        
+        num_params = len(self.initial_params)
+        new_genes = []
+        
+        for gene in best_genes:
+            new_genes.append(gene.get_params())
+        
+        R = np.array(new_genes)
+        m = R.mean()
+        s = R.std()
+        A = (R - m) / s
+        
+        N = np.random.randn(self.parents, num_params) ### MUTATION BASIS
+        
+        params = params + self.learning_rate / (self.parents * self.sigma) * np.dot(N.T, A)
         
         print(rewards, population)
+        
+        return params
